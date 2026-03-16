b083ffb081a9218bd45cf0397a63d3697bb08d76
ryukann <yoannlamouille@gmail.com>
2026-03-16 15:27:37 +0100

    Validation mail

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -17,12 +17,31 @@ function validateForm(){
   const nomOk = validateRequired(inputNom);
   const prenomOk = validateRequired(inputPrenom);
   const mailOk = validateMail(inputMail);
+  const passwordOk = validatePassword(inputPassword);
 
 
-  if(nomOk && prenomOk && mailOk){
+  if(nomOk && prenomOk && mailOk && passwordOk){
     btnValidation.disabled = false;
   }else{
     btnValidation.disabled = true;
+  }
+}
+
+function validatePassword(input){
+  //Définir mon regex
+  const passwordRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+  const passwordUser = input.value;
+  if(passwordUser.match(passwordRegex)){
+    //C'est ok
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+
+  }else{
+    //Il y a une erreur
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    return false;
   }
 }
 
